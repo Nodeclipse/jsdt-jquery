a3f8a7ae481f6eb9f7a0c768bf6426d842f01755
philippe.marschall@gmail.com <philippe.marschall@gmail.com@a3452c27-06eb-3189-26e2-e8349dc22c0e>
2011-05-29 12:11:21 +0000

    - fix license headers

--- org.eclipselabs.jsdt.jquery.ui/src/org/eclipselabs/jsdt/jquery/ui/JQueryUiMessages.java
+++ org.eclipselabs.jsdt.jquery.ui/src/org/eclipselabs/jsdt/jquery/ui/JQueryUiMessages.java
@@ -1,16 +1,15 @@
 /*
- * @(#) $CVSHeader:  $
+ * *****************************************************************************
+ * Copyright (c) 2011 Philippe Marschall and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * Copyright (C) 2011 by Netcetera AG.
- * All rights reserved.
+ * Contributors:
+ *     Philippe Marschall
+ * *****************************************************************************
  *
- * The copyright to the computer program(s) herein is the property of
- * Netcetera AG, Switzerland.  The program(s) may be used and/or copied
- * only with the written permission of Netcetera AG or in accordance
- * with the terms and conditions stipulated in the agreement/contract
- * under which the program(s) have been supplied.
- *
- * @(#) $Id: codetemplates.xml,v 1.5 2004/06/29 12:49:49 hagger Exp $
  */
 package org.eclipselabs.jsdt.jquery.ui;
 
