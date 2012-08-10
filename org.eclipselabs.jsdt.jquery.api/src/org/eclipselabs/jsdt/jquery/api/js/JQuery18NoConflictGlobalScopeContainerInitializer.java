33d9a7117356e315c69dc0a43a51423caab64687
philippe.marschall@gmail.com <philippe.marschall@gmail.com@a3452c27-06eb-3189-26e2-e8349dc22c0e>
2012-08-10 20:46:35 +0000

    - bump 1.8 extension points

---
+++ org.eclipselabs.jsdt.jquery.api/src/org/eclipselabs/jsdt/jquery/api/js/JQuery18NoConflictGlobalScopeContainerInitializer.java
@@ -0,0 +1,26 @@
+/*
+ * *****************************************************************************
+ * Copyright (c) 2011 Philippe Marschall and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Philippe Marschall
+ * *****************************************************************************
+ *
+ */
+package org.eclipselabs.jsdt.jquery.api.js;
+
+import org.eclipselabs.jsdt.jquery.api.SimpleVersion;
+
+
+public class JQuery18NoConflictGlobalScopeContainerInitializer extends
+JQueryGlobalScopeContainerInitializer {
+
+  public JQuery18NoConflictGlobalScopeContainerInitializer() {
+    super(SimpleVersion.fromString("1.8"), true);
+  }
+
+}
