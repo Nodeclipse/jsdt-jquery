e72da7a6fca2dda5b1284891e487258768b30b86
philippe.marschall@gmail.com <philippe.marschall@gmail.com@a3452c27-06eb-3189-26e2-e8349dc22c0e>
2013-02-27 09:45:31 +0000

    make 1.9 work

---
+++ org.eclipselabs.jsdt.jquery.api/src/org/eclipselabs/jsdt/jquery/api/js/JQuery19ConflictGlobalScopeContainerInitializer.java
@@ -0,0 +1,26 @@
+/*
+ * *****************************************************************************
+ * Copyright (c) 2011 Philippe Marschall and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Philippe Marschall
+ * *****************************************************************************
+ *
+ */
+package org.eclipselabs.jsdt.jquery.api.js;
+
+import org.eclipselabs.jsdt.jquery.api.SimpleVersion;
+
+
+public class JQuery19ConflictGlobalScopeContainerInitializer extends
+JQueryGlobalScopeContainerInitializer {
+
+  public JQuery19ConflictGlobalScopeContainerInitializer() {
+    super(SimpleVersion.fromString("1.9"), false);
+  }
+
+}
